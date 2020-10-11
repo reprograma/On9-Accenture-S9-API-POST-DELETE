570fc6adc09d836c392f0dae1f3c1d79ef9f3b25
luanaCristina <luanacrystinne@msn.com>
2020-10-11 01:31:21 -0300

    Alteração do nome novoData para novaData que coloquei errado no arquivo posts-controller.js e por isso estava dando erro ao criar

--- src/controllers/posts-controller.js
+++ src/controllers/posts-controller.js
@@ -1,69 +1,66 @@
-const postModel = require('../models/posts-models');
-const helper = require('../helpers/helper');
+const postModel = require("../models/posts-models");
+const helper = require("../helpers/helper");
 
-const obterPosts = (requisicao, resposta) =>{
+const obterPosts = (requisicao, resposta) => {
   resposta.status(200).json(postModel);
-}
+};
 
 // podem utilizar o getById
 //www.reprograma.com.br/perfil/:id
-const obterIdPost =  (requisicao, resposta) =>{
+const obterIdPost = (requisicao, resposta) => {
   const { id } = requisicao.params;
-  const post = postModel.find(post => post.id == id);
+  const post = postModel.find((post) => post.id == id);
 
   resposta.json(post);
-
-}
+};
 
 // getByTitle
 // resquest, response
 //req, res
 //www.reprograma.com.br/titulo?titulo=Ler
-const obterTituloPost =  (requisicao, resposta) => {
+const obterTituloPost = (requisicao, resposta) => {
   const { titulo } = requisicao.query;
-  const baseDeDados = postModel.find(post => post.titulo == titulo);
+  const baseDeDados = postModel.find((post) => post.titulo == titulo);
 
   resposta.json(baseDeDados);
+};
 
-}
+const criarPost = (requisicao, resposta) => {
+  const { titulo, conteudo, etiqueta } = requisicao.body;
 
- const criarPost = (requisicao, resposta) =>{
- 
-   const { titulo, conteudo, etiqueta } = requisicao.body;
+  const novoPost = {
+    //utilizando o helper
+    id: helper.obterNovoValor(postModel),
+    titulo: titulo,
+    conteudo: conteudo,
+    etiqueta: etiqueta,
+    //utilizando o helper
+    dataCriacao: helper.novaData(postModel),
+  };
 
-   const novoPost ={
-     //utilizando o helper
-     id:  helper.obterNovoValor(postModel),
-     titulo: titulo,
-     conteudo: conteudo,
-     etiqueta: etiqueta, 
-     //utilizando o helper
-     dataCriacao: helper.novoData(postModel)
-   }
+  postModel.push(novoPost);
 
-   postModel.push(novoPost);
+  resposta.status(201).json(novoPost);
+};
 
-   resposta.status(201).json(novoPost);
- }
-
- const deletarPost = (requisicao, resposta) =>{
+const deletarPost = (requisicao, resposta) => {
   const { id } = requisicao.params;
 
-  let postsFiltrados = postModel.filter(post => {
+  let postsFiltrados = postModel.filter((post) => {
     return post.id == id;
   })[0];
 
   const index = postModel.indexOf(postsFiltrados);
-  
-  postModel.splice(index, 1)
 
-  resposta.json(postModel)
- }
+  postModel.splice(index, 1);
 
-module.exports ={
+  resposta.json(postModel);
+};
+
+module.exports = {
   obterPosts,
   obterIdPost,
-  obterTituloPost, 
+  obterTituloPost,
   criarPost,
-  deletarPost
-}
+  deletarPost,
+};
